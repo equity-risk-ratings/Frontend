6165c256ccc4e0c797ad72d748adba271b8384a7
johnschneider1 <johnlawschneider@gmail.com>
2019-07-29 15:13:22 -0500

    working on trading card/ setup not functioning yet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TradeDis from "./components/TradeDis";
 
 function App() {
   return (
     <div className="App">
       <h1>App.js</h1>
+      <TradeDis />
     </div>
   );
 }
