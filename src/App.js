3b2c0fb8093fc2afb754756db75bdcebe06de795
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-29 15:45:41 -0500

    Adding Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <h1>App.js</h1>
+      <header>Equity Risk Ratings</header>
+      {/* <Route exact path='/' component={Home} /> */}
+      <Route path='/signin' component={SignIn} />
+      <Route path='/signup' component={SignUp} />
     </div>
   );
 }
