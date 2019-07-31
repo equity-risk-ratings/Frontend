efc810b1e5d11754e171fdf7d37ffd78df9ffd90
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-30 19:45:51 -0500

    Update TradeCardOutput.js

--- src/components/TradeCardOutput.js
+++ src/components/TradeCardOutput.js
@@ -3,20 +3,20 @@ import { Card, Image } from "semantic-ui-react";
 
 export default function TradeCardOutput({ cryp }) {
   console.log("testProps: ", cryp);
-   (
-    d>
-    mage src={cryp.image} wrapped ui={false} />
-      <Card.Content> 
-      <Card.Header>{cryp.name}</Card.Header>
+  return (
+    <Card>
+      <Image src={cryp.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{cryp.name}</Card.Header>
         <Card.Meta>{cryp.species}</Card.Meta>
         <Card.Description>{cryp.location.name}</Card.Description>
       </Card.Content>
       {/* <Card.Content extra>
-        <a href='#'>
-           <Icon name="user" />
+        <a>
+          <Icon name="user" />
           episodes
         </a>
       </Card.Content> */}
     </Card>
   );
-}
+};
