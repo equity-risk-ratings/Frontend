0cb92ecd1eccfcf6b7f5446422ed54e2bfc3d99d
Ben Allen <BenAllen@Vivians-MacBook-Pro-2.local>
2019-07-31 11:17:03 -0500

    Added basic styling to Hard data - imported plotly

--- src/components/DataHardData.js
+++ src/components/DataHardData.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
+//This is a mock API - NOT for MVP
 import data from '../dummyData';
+
+//These are just basic styles to give the developer visual aide.
+import './basicStyles/DataHardData.css';
 
 const DataHardData = () => {
     return (
         <div className="stockContainer">
+        {/*Maps over the data from API and create divs displays said data*/}
         {data.map((stock) => {
             return (
-            <section>
-                <div>{stock.stockName}</div>
-                <div>{stock.stockPrice}</div>
-                <div>{stock.changeDollar}</div>
-                <div>{stock.changePercent}</div>
+            <section className="container">
+                <div className="stockName">{stock.stockName}</div>
+                <div className="stockPrice">{stock.stockPrice}</div>
+                <div className="changeDollar">{stock.changeDollar}</div>
+                <div className="changePercent">{stock.changePercent}</div>
             </section>
             )
         })}
