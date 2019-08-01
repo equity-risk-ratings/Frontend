2b5fd84e9f4492111e7b1c5182414881190879fd
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-01 17:49:33 -0500

    Merge conflicts resolved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import './App.css';
-import TradeDis from "./components/TradeDis";
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
-=======
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
@@ -14,18 +6,14 @@ import TradingHome from "./components/TradingHome";
 import SignIn from "./components/signin/SignIn";
 import SignUp from "./components/signup/SignUp";
 import Home from "./components/home/Home";
-import Quote from './components/quote/Quote';
-import UserProfile from './components/UserProfile';
->>>>>>> d6de4f51da9f2765330147facc9342d3e97de7d4
+import Quote from "./components/quote/Quote";
+import UserProfile from "./components/UserProfile";
 
-import Data from './Data';
-import automatedDataset from './components/dataMap';
+import Data from "./Data";
+import automatedDataset from "./components/dataMap";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Data />
-=======
       <Router>
         <div>
           <Switch>
@@ -38,7 +26,6 @@ function App() {
           </Switch>
         </div>
       </Router>
->>>>>>> d6de4f51da9f2765330147facc9342d3e97de7d4
     </div>
   );
 }
