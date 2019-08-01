478abadf720132d0d3b7176ff6f97bba7bfc407a
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-31 19:44:51 -0500

    Update Test.js

--- src/components/test-component/Test.js
+++ src/components/test-component/Test.js
@@ -1,93 +1,84 @@
-import React from "react";
-import axios from 'axios';
-import { Statistic, Segment } from 'semantic-ui-react';
-import './ReactVis.css';
+import _ from 'lodash';
+import React from 'react';
+import axios from "axios";
+import { Search, Grid, Segment, Header } from 'semantic-ui-react';
 
-function ReactVis() {
-  const [state, setState] = React.useState({});
+const initialState = {
+  isLoading: false,
+  results: [],
+  value: '',
+  options: [],
+}
 
-  React.useEffect(() => {
+/* const source = _.times(10, () => ({
+  name: '',
+  symbol: '',
+  marketOpen: '',
+  marketClose: '',
+})) */
+
+export default class Test extends React.Component {
+  state = initialState
+
+  componentDidUpdate() {
     axios
-      .get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GOOGL&apikey=LKWC7HB4USLTPXIL')
+      .get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${this.state.value}&apikey=LKWC7HB4USLTPXIL`)
       .then(res => {
-        setState(res.data['Global Quote']);
-        console.log('%cState was successfully set 👍', 'color: green;')
+        this.setState({ options: res.data['bestMatches'] });
       })
       .catch(err => {
-        console.log('Error Caught:', err);
+        console.log('ERROR', err);
       });
-  }, []);
+  }
 
-  return (
-    <Segment.Group compact raised className='segGroup'>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['01. symbol']}</Statistic.Value>
-          <Statistic.Label>Ticker Symbol</Statistic.Label>
-        </Statistic>
-      </Segment>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['02. open']}</Statistic.Value>
-          <Statistic.Label>Open</Statistic.Label>
-        </Statistic>
-      </Segment>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['03. high']}</Statistic.Value>
-          <Statistic.Label>High</Statistic.Label>
-        </Statistic>
-      </Segment>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['04. low']}</Statistic.Value>
-          <Statistic.Label>Low</Statistic.Label>
-        </Statistic>
-      </Segment>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['05. price']}</Statistic.Value>
-          <Statistic.Label>Price</Statistic.Label>
-        </Statistic>
-      </Segment>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['06. volume']}</Statistic.Value>
-          <Statistic.Label>Volume</Statistic.Label>
-        </Statistic>
-      </Segment>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['07. latest trading day']}</Statistic.Value>
-          <Statistic.Label>Latest Trading Day</Statistic.Label>
-        </Statistic>
-      </Segment>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['09. change']}</Statistic.Value>
-          <Statistic.Label>Change</Statistic.Label>
-        </Statistic>
-      </Segment>
-      <Segment>
-        <Statistic horizontal color='teal'>
-          <Statistic.Value >{state['10. change percent']}</Statistic.Value>
-          <Statistic.Label>Change Percentage</Statistic.Label>
-        </Statistic>
-      </Segment>
-    </Segment.Group>
-  );
-};
-/* const data = {
-    symbol: state['01. symbol'],
-    open: state['02. open'],
-    high: state['03. high'],
-    low: state['04. low'],
-    price: state['05. price'],
-    volume: state['06. volume'],
-    latestTradingDay: state['07. latest trading day'],
-    previousClose: state['08. previous cloase'],
-    change: state['09. change'],
-    changePercent: state['10. change percent'],
-  } */
+  handleResultSelect = (e, { result }) => this.setState({ value: result.title })
 
-export default ReactVis;
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.setState(initialState)
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = result => re.test(result.title)
+
+      this.setState({
+        isLoading: false,
+        results: _.filter(/* source */this.state.options, isMatch),
+      })
+    }, 300)
+  }
+
+  render() {
+    const { isLoading, value, results } = this.state
+
+    return (
+      <Grid>
+        <Grid.Column width={6}>
+          <Search
+            loading={isLoading}
+            onResultSelect={this.handleResultSelect}
+            onSearchChange={_.debounce(this.handleSearchChange, 500, {
+              leading: true,
+            })}
+            results={results}
+            value={value}
+            {...this.props}
+          />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Segment>
+            <Header>State</Header>
+            <pre style={{ overflowX: 'auto' }}>
+              {JSON.stringify(this.state, null, 2)}
+            </pre>
+            <Header>Options</Header>
+            <pre style={{ overflowX: 'auto' }}>
+              {JSON.stringify(/* source */this.state.options, null, 2)}
+            </pre>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
