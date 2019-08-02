43fd660116a948732cb4c532a51779e96811ea71
Ben Allen <BenAllen@Vivians-MacBook-Pro-2.local>
2019-08-01 21:48:17 -0500

    Imported nav bar

--- src/Data.js
+++ src/Data.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import DataHardData from "./components/DataHardData";
 import Chart from "./components/Chart";
+import TradeNav from "./components/TradeNav";
 
 import "./components/basicStyles/Data.css";
+
 
 const Data = () => {
 
   return (
     <section className="dataDataContainer">
+      <TradeNav />  
       <DataHardData />
       <Chart />
     </section>
